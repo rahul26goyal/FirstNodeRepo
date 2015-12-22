148f858900d62667dda6b2888db817b81c5decb2
Rahul Goyal <rahul.goyal@xome.com>
2015-12-22 13:16:21 +0530

    Heroku dynamic port config added

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("It's alive!");
   response.end();
-}).listen(3000);
+}).listen(process.env.PORT || 3000);
 
 console.log('server started at post : 3000');
